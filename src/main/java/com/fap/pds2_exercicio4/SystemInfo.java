4d3ec0c7496d15df5b0fc6639e9dbcb5e651f0a3
GilbertWillian <gilbertowillian99@gmail.com>
2020-04-27 00:19:08 -0300

    primeiro commit

---
+++ src/main/java/com/fap/pds2_exercicio4/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.fap.pds2_exercicio4;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
